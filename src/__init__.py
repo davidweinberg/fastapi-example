5c7c14b0a268e2304e00094f6c305d489810a59f
David Weinberg <david.weinberg@anlayzere.com>
2024-08-29 21:58:19 -0300

    added postgres and sqlmodel

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
+from contextlib import asynccontextmanager
+from src.db.main import init_db
 
+# fast api server
 version = "v1"
+@asynccontextmanager
+async def life_span(app: FastAPI):
+    print(f"server is starting ... ")
+    await init_db()
+    yield
+    print(f"server has been stopped")
 
 app = FastAPI(
     title="Bookly",
     description="A REST API for a book review web service",
-    version=version
+    version=version,
+    lifespan=life_span
 )
 app.include_router(book_router, prefix="/api/{version}/books", tags=["Books"])
